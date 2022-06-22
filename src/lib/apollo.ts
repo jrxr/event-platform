379fcf7afdaf40174c36a0f35c9d8a2cf39493a6
Agacy junior <juniormartinssom@gmail.com>
2022-06-22 08:39:48 -0300

    Alterações no apollo

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o20hsr068g01xifeo594im/master',
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
 });
