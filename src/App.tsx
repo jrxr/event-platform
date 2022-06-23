cc5bc0750b620b55362bdd2e9d7f4b34ab56b35c
Agacy junior <juniormartinssom@gmail.com>
2022-06-23 08:23:33 -0300

    Rotas e componente video finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { client } from "./lib/apollo";
 import { Event } from "./Pages/Event";
+import { Router } from "./Router";
 
 function App() {
-
   return (
-    <div>
-      <Event />
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
