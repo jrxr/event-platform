39ca6a0894b493a63e85cdc3bfc1a834833ec145
Agacy junior <juniormartinssom@gmail.com>
2022-06-23 08:53:31 -0300

    Aula 3

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -5,16 +5,63 @@ import {
   FileArrowDown,
   Lightning,
 } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
 
 import "@vime/core/themes/default.css";
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    };
+  };
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug,
+    },
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="A28hMd9uaOg" />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -23,29 +70,24 @@ export function Video() {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do Ignite Lab
-            </h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Chegamos na metade do nosso evento, mas ainda tem mais pela
-              frente…Na terceira aula vamos continuar nosso projeto,
-              desenvolvendo o roteamento e player. Essa é mais uma etapa para
-              sua especialização em React!
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/jrxr.png"
+                src={data.lesson.teacher.avatarURL}
                 alt=""
               />
 
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block">
-                  Junior Martins
+                  {data.lesson.teacher.name}
                 </strong>
                 <span className="text-gray-200 text-sm block">
-                  Developer Front-End
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4o20hsr068g01xifeo594im/master",
+  // uri: "",
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT;
   cache: new InMemoryCache(),
 });
